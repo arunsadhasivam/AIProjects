b6a59b79b84393cc92e71bbbf35c40af0683876f
arun <arunsadhasivam@yahoo.co.in>
2024-08-08 19:14:19 -0700

    Create pycharmsetup.bat

---
+++ 1.Installation/pycharmsetup.bat
@@ -0,0 +1,7 @@
+Step1:
+======
+
+go to pycharm > customize > AllSettings > python Interpreter > Add Interpreter > Add local interpreter
+
+choose > virtual Environment > conda Executable > C:\Anaconda3\condabin\conda.bat- > click load Executable > use existing Environment > pytorch
+
